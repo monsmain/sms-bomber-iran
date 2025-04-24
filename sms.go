c20e78d0ae2dbe10b9c03aacbdda43166871cbd5
monsmain <matinfarmani20@gmail.com>
2025-04-24 19:53:40 +0330

    Update sms.go

--- sms.go
+++ sms.go
@@ -848,7 +848,7 @@ func main() {
 
 	}
 
-	for i := 0; i < repeatCount*183; i++ {
+	for i := 0; i < repeatCount*225; i++ {
 		statusCode := <-ch
 		if statusCode == 404 || statusCode == 400 {
 			fmt.Println("\033[01;31m[-] Error ! ")
