1d140e1d485fff1a7ca9fd93efe6411f7792a732
monsmain <matinfarmani20@gmail.com>
2025-04-25 11:28:29 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -253,16 +253,11 @@ func main() {
 
 	}
 
-	for i := 0; i < repeatCount*; i++ {
-		statusCode := <-ch
-		if statusCode == 404 || statusCode == 400 {
-			fmt.Println("\033[01;31m[-] Error ! ")
-		} else {
-			fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
-		}
-
-	}
+for i := 0; i < repeatCount; i++ {
+    statusCode := <-ch
+    if statusCode == 404 || statusCode == 400 {
+        fmt.Println("\033[01;31m[-] Error ! ")
+    } else {
+        fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
+    }
 }
-/*
-END
-/8
