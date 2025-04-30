2fca6b1a884bbc29fb40ddf5458bdbce6e3e7252
monsmain <matinfarmani20@gmail.com>
2025-04-30 20:04:51 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -125,6 +125,314 @@ asangem.com
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://mashinbank.com/api2/users/check
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.234.89:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{mobileNumber: "09123456456"}
+mobileNumber
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://takfarsh.com/wp-admin/admin-ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+5.9.154.241:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: voorodak__submit-username
+username: 09123456456
+security: 6e887b3a6d
+---------------------------------------------------------------------------------------------
+Request URL:
+https://dicardo.com/sendotp
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+172.67.217.3:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+csrf_dicardo_name: 225a834ed22125cc08474f311f86c65a
+phone: 09123456456
+type: 0
+codmoaref: 
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://bit24.cash/auth/api/sso/v2/users/auth/check-user-registered?country_code=98&mobile=09123456456
+Request Method:
+GET
+Status Code:
+200 OK
+Remote Address:
+104.22.48.184:443
+Referrer Policy:
+same-origin
+
+country_code: 98
+mobile: 09123456456
+
+
+2:
+Request URL:
+https://bit24.cash/auth/api/sso/v2/users/auth/register/send-code
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+104.22.48.184:443
+Referrer Policy:
+same-origin
+
+{country_code: "98", mobile: "09123456456"}
+country_code
+: 
+"98"
+mobile
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://account.bama.ir/api/otp/generate/v4
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+no-referrer
+
+username: 09123456456
+client_id: popuplogin
+---------------------------------------------------------------------------------------------
+Request URL:
+https://lms.tamland.ir/api/api/user/signup
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.226.142.142:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{Mobile: "09128887494", SchoolId: -1, consultantId: "tamland", campaign: "campaign",…}
+Mobile
+: 
+"09128887494"
+SchoolId
+: 
+-1
+campaign
+: 
+"campaign"
+consultantId
+: 
+"tamland"
+utmMedium
+: 
+"wordpress"
+utmSource
+: 
+"tamland"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://api.zarinplus.com/user/otp/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.232.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{phone_number: "989123456456", source: "zarinplus"}
+phone_number
+: 
+"989123456456"
+source
+: 
+"zarinplus"
+
+2:
+Request URL:
+https://firebaseinstallations.googleapis.com/v1/projects/emtiyaz-a20c8/installations/eSeY83hZmWCOYNLj7vmuBU/authTokens:generate
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+63.141.252.205:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{installation: {sdkVersion: "w:0.6.12", appId: "1:622396066313:web:26ccd158dc92b94b7906d8"}}
+installation
+: 
+{sdkVersion: "w:0.6.12", appId: "1:622396066313:web:26ccd158dc92b94b7906d8"}
+appId
+: 
+"1:622396066313:web:26ccd158dc92b94b7906d8"
+sdkVersion
+: 
+"w:0.6.12"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://api.abantether.com/api/v2/auths/register/phone/send
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.232.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{phone_number: "09128887484"}
+phone_number
+: 
+"09128887484"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://bck.behtarino.com/api/v1/users/jwt_phone_verification/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.166.104.3:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+
+{phone: "09123466456"}
+phone
+: 
+"09123466456"
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://web-api.fafait.net/api/graphql
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.229.133.70:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{operationName: "hasUser", variables: {input: {username: "09128887494"}}, extensions: {,…}}
+extensions
+: 
+{,…}
+persistedQuery
+: 
+{version: 1, sha256Hash: "00fbd099cf5cad12af5114cff9e4676649ba70b9c4c6c3d1ebfcd68972bc1a3f"}
+operationName
+: 
+"hasUser"
+variables
+: 
+{input: {username: "09128887494"}}
+input
+: 
+{username: "09128887494"}
+
+2:
+Request URL:
+https://web-api.fafait.net/api/graphql
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.229.133.70:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+
+{variables: {input: {mobile: "09128887494", nickname: "dvxfgdfgd"}}, extensions: {,…}}
+extensions
+: 
+{,…}
+persistedQuery
+: 
+{version: 1, sha256Hash: "c86ec16685cd22d6b486686908526066b38df6f4cbcd29bef07bb2f3b18061e6"}
+variables
+: 
+{input: {mobile: "09128887494", nickname: "dvxfgdfgd"}}
+input
+: 
+{mobile: "09128887494", nickname: "dvxfgdfgd"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
