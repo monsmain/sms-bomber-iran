d6fc5f1f11fe64bb333b850b4a90f856fc6c4db8
monsmain <matinfarmani20@gmail.com>
2025-05-01 10:50:58 +0330

    Update sms.go

--- sms.go
+++ sms.go
@@ -2169,5 +2169,9 @@ cookieJar, _ := cookiejar.New(nil)
 			fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSended")
 		}
 	}
+    client.CloseIdleConnections()
+
+    fmt.Println("\033[01;33m[*] Done sending requests.\033[0m")
+
 }
 //Code by @monsmain
