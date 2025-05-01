2d9528b112dbcb54967cfc937d753b806e553813
monsmain <matinfarmani20@gmail.com>
2025-05-01 17:10:05 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -93,19 +93,453 @@ lintagame.com
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://graphicsara.com/?amzshyar_account_ajax=true
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+65.21.73.109:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+amzshyar_account_ajax: true
+key: auth
+level: first
+r: 
+phone: 9123456456
+---------------------------------------------------------------------------------------------
+Request URL:
+https://tipaxco.com/login
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+86.104.35.110:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+PortalScriptManager: ctl15$ctl03$ctl01$ctl15$ctl03$ctl01$_rapLoginPanel|ctl15$ctl03$ctl01$_lbSendCode
+PortalScriptManager_TSM: ;;System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35:fa-IR:234e29d2-f631-44aa-ae02-1f6a28518d09:ea597d4b:b25378d2;Telerik.Web.UI, Version=2023.1.117.45, Culture=neutral, PublicKeyToken=121fae78165ba3d4:fa-IR:ba768a1f-3205-4bbb-b2c9-b540445080b3:16e4e7cd:ed16cbdc:365331c3:24ee1bba:c128760b:19620875:874f8ea2:c172ae1e:f46195d3:9cdfc6e7:33715776:e330518b:2003d0b8:88144a7a:1e771326:c8618e41:e4f8f289:1a73651d:16d8629e;
+PortalStylesheetManager_TSSM: ;Telerik.Web.UI, Version=2023.1.117.45, Culture=neutral, PublicKeyToken=121fae78165ba3d4:fa-IR:ba768a1f-3205-4bbb-b2c9-b540445080b3:d7e35272:e8a5fa6b:505983de:62e8a487:45085116:27c5704c;Telerik.Web.UI.Skins, Version=2023.1.117.45, Culture=neutral, PublicKeyToken=121fae78165ba3d4:fa-IR:11e5640d-1a0b-40d8-84ba-d228fd474813:c1add914:a2632222:ad4550ef
+__RequestVerificationToken: kAk-6d90Taa1RcFK6hadei5m8_tLcW0o9yaM-svEhtiEe1TL1Ip3b39t65nI-XX1naUzNCcXQKg8RJaaufghYT66ugB5z78rJLZCEhUfKlfsRhvtOHpLE1JUpKx_hDX1TbBXwfg0J5_KHYJCg90O6A2
+ctl15:ctl00:ctl01:ctl00::sbSearch:ctl00: 
+ctl15$ctl00$ctl01$ctl00$_hfarealabel: جستجو
+ctl15$ctl03$ctl01$_txtMobile: 09123456456
+ctl15$ctl03$ctl01$_cbAcceptPrivacyAndPolicy: on
+ctl15_ctl03_ctl01__rnNotification_ClientState: 
+ctl15$ctl01$ctl02$ctl00$m_207926770$ctl01$_txtMail: 
+__EVENTTARGET: ctl15$ctl03$ctl01$_lbSendCode
+__EVENTARGUMENT: 
+__LASTFOCUS: 
+__VIEWSTATE: SynYPHTn5cIRu1hMqlKmc6N82x0nPQAyEdmI2uHiPToyYqP1drJuW4Ps7JYdXSKUFfFu1ActU/HM0Q9Q/vqRkzyVD+IjVOGLPU/d1BeJkFXoL8V712Mo9+IA7VRVCEfAmYM8hjoPeRkLfbk/z4v2UdYNCmqcHpHLuwoT9crCvLyqThXnz2S6C6nRmmTG1FMYeb3JcV8Pdn9Bbf6QUB+/3aV4zCUlTTq/RJhc8WIVTzuUA3k3CoCvz9wBDhMn3/MwlVce7UafXC5lwdhTj60+QomaeYsH0ExEp0dSbG/fNhOdjlAUy+1vPUTJWh1ZO1VFy9EW46GjpRcB8K4EYKDSjSz86pRkYZjq8a99Pil/l2p4cxMdBVRW0QiPEFyubad9cnj1Hxd3J2jz9ss56d8KMGAz3aRjjc5TxwifR1egX/X0zFu84n64d4uwm8HdnX+v+OoVxT9ePFa+HioePtB9ftypEMTuYqhuLBVI8n9kQSY6grZ0Qw6M6L3DkCjKVdkwT/8O4YxwYzh51Q+Eeh/JJPV16Nb/JQIKKiT2jReiwbuUJSRx8s4sUn9nVqf1kv1OpuRelvCHVTUx+VfxU5sbCA7yE6wA1c33P94fKPhav2fAhbJLR6gTw6krrpCQBtmc9WbgdGqj1FNv2AqqhJpsM0GsRpP0LASfp+47WQjKz1jb1mKw6Prmd1QBhyyhhbv0xApfFAptbPlOb/wF9iXKZoBa1oifIPQPViWOtPGGmFmpSKAAQWNTJbXkTF7gkH1p3VuPoNRyabU0CTfj1rhmeH5dtuBH0Q6ZHVD0Wh/CE4oyBqpdI+qiDUziZtDlgpBuZobGX6hngFHICyv/ctW7PxskK0raGftBIPztrOCjMGpLQyldFANK54mfqcdnYHWW4bozGLH9p8w8d+5QAcu2X5VuI7cPb6YOXgZbCO3BI6ByvgquWGnz0C5PRT4Fgd2xloxkCpjZUkG+aMU0hPVdnxRKCtxuxiKbQ1pUN//izeUXEOFijIkydgWMarNUWOo9w5ilbse6Hrn43HPN2qin97TsIYSR3P7rwylSPZ1CNUe+6vkMfYcrjbG+nSoG0ZqWnIDx6gLCdOTNQw9JGCTlqgScLDCQPgN/bQN2+RtUVfY9AkhHWQc/TgRZAg/vmY6QpAxSlTTiwiyiI1x/Z7scQ6dhZhwCR+y26VWkxUMbSFLPj669MGko+KkoSHqLWEqjaBatzDkZlpTyLQkRWxq902f3X3JF6uCBjaJYlp6lyRd5j3tBhzr5jIcb+FK0JGnbMqN1a/rc9p5I45505wInCh1Ix2pmyBLOXnWSLJdX8XX9kvk1SqQ3Hmuk3jn/i2+W/6QuVTluh+3wHKnzKMb5Osh50v6UQvF0l/y6Y93VtJanJK9F0AMcVLOJXeo5fke0/hdtbqs/uLV3Rx3tMSnK3PKLoaa1WkIWTkzZ/ekUALxwkC8vuORjF+uth3TG8mmd0xv5esv7PZQXZoTnjdj5Ge44yEMG9wmhFbwG953u1vKpFWJAKOemcsTh5GbgnE8dpvB4KYEg2XjIrmuLCCapZY8dycpyOGKtUsPY9zgPdjZynSNVw+IFFSoSJOMWnSDpVhu95TvRbF5g0qsGbvHIdqBfmtO7J8e1sA+iIg7B5/BIl85A8c4EK4UVdNg599OO7TTkul3vp1t4thumCeN4ERDCDPovJJ+DPC5Z17Ouc8jCQxe4DW4pdNOMLdPb7CuVFOeNKyttYhd48op1duUo3CeB9cM5y/aUOGXHtq/w6xOYZmDen+kAeHABE9LL5CtEhHVFpivwf/6Zo6Pa9kT6u+seKDBIX+LInVuuBQ/Ypk4D66hS5Hmn369faSuTMksqJczelihyiXEgOoFAdFvHJamyQtXLJuTvcw2HglkLMs4W5bj310RnecYHSu8z0Pnwkh/n1B0yOZdc/75bLItxtR1ttgRAI7DL3aS9D/DSvcE=
+__VIEWSTATEGENERATOR: CA0B0334
+ctl15$ctl03$ctl01$_rnNotification$hiddenState: 
+ctl15_ctl03_ctl01__rnNotification_XmlPanel_ClientState: 
+ctl15_ctl03_ctl01__rnNotification_TitleMenu_ClientState: 
+__ASYNCPOST: true
+RadAJAXControlID: ctl15_ctl03_ctl01__rapLogin
+
+---------------------------------------------------------------------------------------------
+Request URL:
+https://api.nikpardakht.com/api/v1/register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.166.104.3:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{mobile: "09123456999", type: "natural", endPointType: "v1/register"}
+endPointType
+: 
+"v1/register"
+mobile
+: 
+"09123456999"
+type
+: 
+"natural"
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://www.flytoday.ir/api/collect
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{plaintext: "09123456456"}
+plaintext
+: 
+"09123456456"
+2:
+Request URL:
+https://api.flytoday.ir/api/V3/User/Register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{email: "gudfdihsk@gmail.com", mobile: "URY0+oOlpTzhxPzeTL6Chg==", password: "vounhchvdim2",…}
+email
+: 
+"gudfdihsk@gmail.com"
+mobile
+: 
+"URY0+oOlpTzhxPzeTL6Chg=="
+password
+: 
+"vounhchvdim2"
+registerReason
+: 
+0
+---------------------------------------------------------------------------------------------
+Request URL:
+https://noornegar.com/login-request
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+144.76.119.187:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
 
+mobile: 09123456456
+_token: FJqxrn8QM6ryv9UTtyzdhvnLxwzzoiN1RNnOYjyW
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://baziket.com/baziket-server/plugins/supportboard/supportboard/include/ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+195.28.10.56:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+function: ajax_calls
+calls[0][function]: get-front-settings
+calls[0][current_url]: https://baziket.com/my-account/
+calls[0][tickets]: false
+calls[0][popup]: false
+login-cookie: V1hPTndmQzRBQXBwVG55a0s1YXNqQkFCUkZhRDVGemJqUS8zNTVOeXpNa1YzenJ1ZCtBakxlc1h4VkdDRGI4bXFnTEpiUFg2ZmszU2FCZ2M4bkU2QnlDN0sySXl0U2N1NzNGYVNmRi9HVGZROWI2dmsvU0UrK0xKNExVUytramxYWkpNeDFFemErT0xPUUJkamI4T1krUjNJUkM2VVZjWFZLL3JSWW4zUTg1TmQ1LzhCOXFmZk5PaElWVFc0elRqOS9CblROclNrU3RuZlhBTWpORGsrVkdTSkpyKzNUV3BGNlg2bjJvN0diS1laQXNTUkRXVk5TQXlNdGI4RG0zU2lDR0lzMFZRWFpCakEvRVMwcXd6dlJYcWFPYWpTUUZiQTFOL2xjaGpyc1p1V2lxeGg2TDJtOWx6NWVBTWY4Q1IrcnpNWWRqaFI1UzZ4TWF4aVBnNFJXVGVScUEzYzQzTDJZUlNGdjM0dElLNysrUzlmcVhSS1poZGhMbmdoMU1aams4QmpRam03NFQxRVdQVDFsRVRnMlR0UDFEOHlHR1ZmWDVNNitFdTNJMDJ4YllQYVJMWnRhMzZKRHRkWWlsWS9mSkN1T1lleHJldkxUZjlnV1gyRXl6UTFra21qNjRkTkRNUWVjcDg1amc9
+language[]: fa
+language[]: front
+
+2:
+Request URL:
+https://baziket.com/wp-admin/admin-ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+195.28.10.56:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: digits_check_mob
+countrycode: +98
+mobileNo: 09123456456
+csrf: 0660ca284b
+login: 2
+username: 
+email: koyakef766@kazvi.com
+captcha: 
+captcha_ses: 
+json: 1
+whatsapp: 0
+---------------------------------------------------------------------------------------------
+Request URL:
+https://api.3click.com/auth/validate
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.232.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{mobile: "09123456456"}
+mobile
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://dekomaj.com/bakala/ajax/send_code/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+31.25.90.147:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: bakala_send_code
+phone_email: 09123466456
+---------------------------------------------------------------------------------------------
+Request URL:
+https://www.hamrah-mechanic.com/api/v1/membership/otp
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.232.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{PhoneNumber: "09123456456", prevDomainUrl: "https://nazarkade.com/",…}
+PhoneNumber
+: 
+"09123456456"
+landingPageUrl
+: 
+"https://www.hamrah-mechanic.com/?utm_medium=company_profile&utm_source=nazarkade.com&utm_campaign=domain_click"
+orderPageUrl
+: 
+"https://www.hamrah-mechanic.com/membersignin/"
+prevDomainUrl
+: 
+"https://nazarkade.com/"
+prevUrl
+: 
+null
+referrer
+: 
+"https://nazarkade.com/"
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://styleup.ir/wp-content/plugins/styleup-login-register_e/include/custom-ajax-handler/ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+79.127.127.97:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: verify_user_login
+fast_ajax: true
+user: 09123456456
+nonce: 2b0cab7038
+duty: check_user
+
+2:
+Request URL:
+https://styleup.ir/wp-content/plugins/styleup-login-register_e/include/custom-ajax-handler/ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+79.127.127.97:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: verify_user_login
+fast_ajax: true
+user: 09123456456
+nonce: 2b0cab7038
+duty: send_otp_to_user
+---------------------------------------------------------------------------------------------
+Request URL:
+https://fanpooya.com/bakala/ajax/send_code/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+45.139.11.55:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: bakala_send_code
+phone_email: 09123466456
+---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://api2.eligasht.com/api/account/user-check
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+origin-when-cross-origin
+
+{userName: "09123456456", isAgency: false}
+isAgency
+: 
+false
+userName
+: 
+"09123456456"
+
+2:
+Request URL:
+https://api2.eligasht.com/api/account/register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+origin-when-cross-origin
+
+{userName: "09123456456"}
+userName
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://api.technosun.ir/v1/auth/request-otp
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+94.182.98.195:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{call: "0", mobile: "09128887494", source: "register", type: "register"}
+call
+: 
+"0"
+mobile
+: 
+"09128887494"
+source
+: 
+"register"
+type
+: 
+"register"
+
+---------------------------------------------------------------------------------------------
+Request URL:
+https://nazarkade.com/wp-content/plugins/Archive//api/check.mobile.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.88.177.222:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+countryCode: +98
+mobile: 9121111010
 
 
+2:
+Request URL:
+https://nazarkade.com/wp-admin/admin-ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.88.177.222:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
+action: digits_check_mob
+countrycode: +98
+mobileNo: 9121111010
+csrf: 3a32c241bf
+login: 2
+username: 
+email: 
+captcha: 
+captcha_ses: 
+digits: 1
+json: 1
+whatsapp: 0
+digregcode: +98
+digits_reg_mail: 9121111010
+digits_reg_password: x
+digits_reg_name: x
+dig_otp: 
+code: 
+dig_reg_mail: 
+dig_nounce: 3a32c241bf
+---------------------------------------------------------------------------------------------
+Request URL:
+https://apigateway.fadaktrains.com/api/auth/otp
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+212.16.70.74:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
+{phoneNumber: "09123456456"}
+phoneNumber
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://hoseinifinance.com/?amzshyar_account_ajax=true
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+78.158.166.38:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
+amzshyar_account_ajax: true
+key: auth
+level: first
+r: %2F
+phone: 9123456456
+---------------------------------------------------------------------------------------------
+Request URL:
+https://toprayan.com/account/login
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+5.144.131.168:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
-
-
-
-
-
+Step: EnterMobile
+Mobile: 09123456456
+Password: 
+RememberMe: false
+VerifyCode: 
+X-Requested-With: XMLHttpRequest
+---------------------------------------------------------------------------------------------
 
 
 
