80392a4104c22b0292ea7f020c946458b5171489
monsmain <matinfarmani20@gmail.com>
2025-05-04 00:01:25 +0330

    Update sms.go

--- sms.go
+++ sms.go
@@ -665,11 +665,11 @@ cookieJar, _ := cookiejar.New(nil)
         Timeout: 10 * time.Second,
 	}
 
-	tasks := make(chan func(), repeatCount*140)
+	tasks := make(chan func(), repeatCount*142)
 
 	var wg sync.WaitGroup
 
-	ch := make(chan int, repeatCount*140)
+	ch := make(chan int, repeatCount*142)
 
 	for i := 0; i < numWorkers; i++ {
 		go func() {
