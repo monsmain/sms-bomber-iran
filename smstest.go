6d5d65d740105b2bfd8082c692a13b3bd76cdea0
monsmain <matinfarmani20@gmail.com>
2025-05-03 23:43:41 +0330

    Update smstest.go

--- smstest.go
+++ smstest.go
@@ -1,12 +1,413 @@
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+Request URL:
+https://naabshop.com/wp-admin/admin-ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+164.138.20.121:443
+Referrer Policy:
+strict-origin-when-cross-origin
 
+
+login_digt_countrycode: +98
+digits_phone: 912 345 6456
+action_type: phone
+digits_reg_name: efetgrt
+digits_reg_lastname: htttgtrtr
+digits_process_register: 1
+sms_otp: 
+otp_step_1: 1
+signup_otp_mode: 1
+rememberme: 1
+digits: 1
+instance_id: 27744fbc0c69e6e612567dd63636fde4
+action: digits_forms_ajax
+type: login
+digits_step_1_type: 
+digits_step_1_value: 
+digits_step_2_type: 
+digits_step_2_value: 
+digits_step_3_type: 
+digits_step_3_value: 
+digits_login_email_token: 
+digits_redirect_page: //naabshop.com/?utm_medium=company_profile&utm_source=nazarkade.com&utm_campaign=domain_click
+digits_form: 28e10ee7bd
+_wp_http_referer: /?utm_medium=company_profile&utm_source=nazarkade.com&utm_campaign=domain_click
+show_force_title: 1
+container: digits_protected
+sub_action: sms_otp
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://kimiaonline.com/Customer/ApiLoginFull
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+62.204.61.222:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+mobile: 09123456456
+ReturnUrl: 
+__RequestVerificationToken: CfDJ8K42OkgRHtZHr-l-HyuyWg_-wMOT-L8MKjIXXpwDj41J63UA70956P2fKvf4lDLshBxs1YEPx-CWO-8e7nQr191ABIMLTosUuNI5bvZKOoo0JOLaG6eg_o_u_OP6AUTORC6h2s6KT_C7lEtOnbqHNFk
+step: 1
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://armaghan90.ir/users/login-register/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.53.143.214:443
+Referrer Policy:
+no-referrer-when-downgrade
+
+username: 09123456456
+csrfmiddlewaretoken: VAdT8eppsOcqblHMfLqVD675mGAwgql62ACAXSBF3iwaxX0vD3xM9zfogiMoicKS
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://numberland.ir/ajax_login.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+172.67.72.39:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+vaziatload: checkfornewentertosite
+enterbyusepass: 1
+XCSRFTOKEN: 6fd6a92d166ee4cd3681c7694523913c98da043490a6319619debd93cf2fb27d5a2be573
+user: 09123456456
+safeme: 1
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://coreapi.bimeh.com/v1/authentication
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.143.235.201:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{MobileNumber: "09123456456"}
+MobileNumber
+: 
+"09123456456"
 ---------------------------------------------------------------------------------------------
+1:
+Request URL:
+https://api-gw.karnameh.com/switch/api/auth/otp/send/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.166.104.3:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{phone_number: "09123456456"}
+phone_number
+: 
+"09123456456"
+
+2:
+Request URL:
+https://api-gw.karnameh.com/user-trace/user-trace/v1/event_logs/register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.166.104.3:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{service_name: "general", event_name: "OTP_RESPONSE", event_type: "submit", event_metadata: {,…},…}
+app_referrer
+: 
+"web"
+attribution_metadata
+: 
+{najva_push_token: null}
+event_metadata
+: 
+{,…}
+event_name
+: 
+"OTP_RESPONSE"
+event_type
+: 
+"submit"
+service_name
+: 
+"general"
+url
+: 
+"https://karnameh.com/?utm_medium=company_profile&utm_source=nazarkade.com&utm_campaign=domain_click"
+utm
+: 
+{campaign_id: "domain_click", utm_medium: "company_profile", utm_source: "nazarkade.com",…}
+viewer_id
+: 
+"c21e647f-1c31-4b72-826b-2a950f2204c2"
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://zanoone.ir/api/auth/register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+193.141.65.49:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{Mobile: "09123456456", FirstName: "abghnyun", LastName: "gyhttrd", Gender: "false",…}
+Email
+: 
+"test@gmail.com"
+FirstName
+: 
+"abghnyun"
+Gender
+: 
+"false"
+LastName
+: 
+"gyhttrd"
+Mobile
+: 
+"09123456456"
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://client.afrak.com/api/v1/pre-register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+37.32.9.88:443
+Referrer Policy:
+no-referrer-when-downgrade
+
+{first_name: "سبتد بیتن", phone_number: "09123456456", password: "لذفبدغعنغدالذا5", code: "",…}
+code
+: 
+""
+first_name
+: 
+"سبتد بیتن"
+invite_id
+: 
+""
+password
+: 
+"لذفبدغعنغدالذا5"
+phone_number
+: 
+"09123456456"
+rules
+: 
+true
 ---------------------------------------------------------------------------------------------
+Request URL:
+https://darukade.com/api/auth/register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.78.21.33:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+{Mobile: "09123456456", FirstName: "ققغغف", LastName: "غغلق", Gender: "true", Email: "tedt@gmail.com"}
+Email
+: 
+"tedt@gmail.com"
+FirstName
+: 
+"ققغغف"
+Gender
+: 
+"true"
+LastName
+: 
+"غغلق"
+Mobile
+: 
+"09123456456"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://irankohan.ir/Register/Register
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+62.60.211.241:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+returnUrl: 
+CompanyName: 
+Name: fefgbggg
+Mobile: 09123456456
+Email: rwrd@gmail.com
+Password: 123456789
+ConfirmPassword: 123456789
+X-Requested-With: XMLHttpRequest
+---------------------------------------------------------------------------------------------
+Request URL:
+https://masterkala.com/api/2.1.1.0.0/?route=profile/otp
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+79.127.127.47:443
+Referrer Policy:
+no-referrer
+
+route: profile/otp
+{type: "sendotp", phone: "09123466456"}
+phone
+: 
+"09123466456"
+type
+: 
+"sendotp"
+---------------------------------------------------------------------------------------------
+Request URL:
+https://taminniaz.com/wp-admin/admin-ajax.php
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+94.232.169.200:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: digits_check_mob
+countrycode: +98
+mobileNo: 09123456456
+csrf: 3ea3440f99
+login: 2
+username: 
+email: koyakef766@kazvi.com
+captcha: 
+captcha_ses: 
+json: 1
+whatsapp: 0
+---------------------------------------------------------------------------------------------
+Request URL:
+https://mo7.ir/bakala/ajax/send_code/
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+178.239.158.110:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+action: bakala_send_code
+phone_email: 09123466456
+---------------------------------------------------------------------------------------------
+Request URL:
+https://iranous.com/login?back=my-account
+Request Method:
+POST
+Status Code:
+200 OK
+Remote Address:
+185.159.153.214:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+back: my-account
+username: 09128887484
+id_customer: 
+back: 
+firstname: بثعاهبتنب
+lastname: ببعباثعبهث
+password: gfgyj56u67urr
+action: register
+back: my-account
+ajax: 1
+---------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
