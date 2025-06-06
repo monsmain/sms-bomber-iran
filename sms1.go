e66a896f9ada42c2424b3b3a8381ec84b6cb4d76
monsmain <matinfarmani20@gmail.com>
2025-06-06 10:27:22 +0330

    Update sms1.go

--- sms1.go
+++ sms1.go
@@ -1,12 +1,25 @@
 /* coded by @monsmain
-⚠️note: copy mamnoe.
-❌befahmam copy kardi khahareto migam...*/
-/* coded by @monsmain
-⚠️note: copy mamnoe.
-❌befahmam copy kardi khahareto migam...*/
-/* coded by @monsmain
-⚠️note: copy mamnoe.
-❌befahmam copy kardi khahareto migam...*/
-/* coded by @monsmain
-⚠️note: copy mamnoe.
-❌befahmam copy kardi khahareto migam...*/
+⚠️NOTE FA:
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+
+⚠️NOTE EN:
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+*/
