396144251620afb8bb0a7e19e8728821da86d36b
monsmain <matinfarmani20@gmail.com>
2025-07-25 10:43:46 +0330

    Update test.go

--- test.go
+++ test.go
@@ -892,11 +892,6 @@ tasks <- func(c *http.Client) func() {
         headers := map[string]string{
             "Host":            "ws.alibaba.ir",
             "User-Agent":     *
-
-
-
-
-
 	}
 
 	close(tasks)
